debcb8408ac57f5cde8176fd1052d0574cf97182
Patrick Arnold <patricka02@ksu.edu>
2023-11-29 22:26:23 -0600

    FIXED NAVIGATION

---
+++ EnrollmentApplication/Pages/CourseHistory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnrollmentApplication.Pages
+{
+    public class Course_HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EnrollmentApplication/Pages/MyTerm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnrollmentApplication.Pages
+{
+    public class My_TermModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
