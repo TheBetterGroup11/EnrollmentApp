a3f93a3f13b8378d14c302312c8c4695e2603257
Patrick Arnold <patricka02@ksu.edu>
2023-11-29 18:50:51 -0600

    BROKEN BRANCH

---
+++ EnrollmentApplication/Pages/MyTerm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnrollmentApplication.Pages.NavBar
+{
+    public class MyTermModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
