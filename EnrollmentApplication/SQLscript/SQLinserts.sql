d5f511a5b4b85deded9b9d11308b21be39f27f7a
Patrick Arnold <patricka02@ksu.edu>
2023-12-01 15:32:20 -0600

    solution1 saves

---
+++ EnrollmentApplication/SQLscript/SQLinserts.sql
@@ -0,0 +1,18 @@
+INSERT INTO Student (StudentId, FirstName, LastName, Grade) VALUES (123, 'Patrick', 'Arnold', 'Senior');
+INSERT INTO Student (StudentId, FirstName, LastName, Grade) VALUES (234, 'Garrett', 'Love', 'Senior');
+
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (115, 'Introduction', 0, 3);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (200, 'Fundamentals', 0, 4);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (300, 'Data Structures', 0, 3);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (301, 'Logic', 0, 3);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (308, 'C Language', 0, 1);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (400, 'Object-Orientated', 0, 3);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (1220, 'Calc 1', 1, 4);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (1221, 'Calc 2', 1, 4);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (1222, 'Calc 3', 1, 4);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (1510, 'Matrix', 1, 3);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (1551, 'Discrete', 1, 3);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (2210, 'Chem 1', 2, 4);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (2230, 'Chem 2', 2, 4);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (3220, 'Expo 1', 3, 4);
+INSERT INTO Course (CourseId, Name, DepartmentId, CreditHours) VALUES (3221, 'Expo 2', 3, 4);
